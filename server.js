59bb16f5072e4258ebabee200dfe84b70fdaaff3
Glitch (productive-unicorn) <none>
2017-12-25 11:30:09 +0000

    Exporting to RedTachyon/fcc-timestamp

---
+++ server.js
@@ -0,0 +1,60 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/:datetime", (req, res) => {
+  let query = req.params.datetime;
+  let time;
+  if (isNaN(query)) {
+    time = Date.parse(query);
+    res.send({
+      unix: time,
+      natural: query
+    });
+  } else {
+    let d = new Date(query*1);
+    res.send({
+      unix: query,
+      natural: d.toUTCString()
+    });
+  }
+  res.end();
+  
+  res.send(time.toString());
+});
+
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
